0ca9221686da03e94d2073ccaf243a4b17824244
RyanFishe <82990731+RyanFishe@users.noreply.github.com>
2023-06-01 23:00:27 -0700

    Add files via upload
    Added an "Empty Cart" feature to the shopping cart screen. Fixed an issue in the categories page not accurately giving the right product ID's.

--- razzle/flask_app/controllers/orders.py
+++ razzle/flask_app/controllers/orders.py
@@ -1,15 +1,20 @@
 from flask_app import app
 from flask import Flask, render_template, request, redirect, session, flash
 from flask_app.models import user, product, category, order
-
+from flask_app.config.mysqlconnection import connectToMySQL
 
 
 @app.route('/place_order', methods=["POST"])
 def create_order():
+
+    if session['cart_total'] == 0:
+        flash("There's nothing in your cart!!")
+        return render_template('cart.html')
+
+
     total_price = session['cart_total']
     user_id = session['user_id']
     print('ORDER PLACED')
-
 
     data = {
         "total_price": total_price,
@@ -33,6 +38,21 @@ def create_order():
     session['cart'] = []
 
     return redirect('/dashboard')
+
+
+@app.route('/cart')
+def cart_home():
+    return render_template("cart.html")
+
+#empties the cart
+@app.route('/empty_cart', methods=["POST"])
+def empty_cart():
+
+    session['cart_total'] = 0
+    session['cart'] = []
+
+    return redirect('/cart')
+
 
 @app.route('/my_orders')
 def view_my_orders():
@@ -74,3 +94,61 @@ def view_order_details(id):
     return render_template('view_order_details.html', order_list = order_list, full_order = full_order )
 
 
+# @app.route('/add_order')
+# def show_oder_page():
+
+#     if 'admin' not in session:
+#         flash("Must be logged in as admin!!")
+#         return redirect('/dashboard')
+#     else:
+#         all_categories = category.Category.get_all()
+#         all_orders = order.Order.get_all()
+#         return render_template('add_order.html', all_orders=all_orders, all_categories=all_categories)
+
+
+# @app.route('/edit_order', methods=["POST"])
+# def edit_order():
+
+#     if not order.Order.validate_order(request.form):
+#         return redirect(request.referrer)
+#     data = {
+
+#         "name": request.form['name'],
+#         "description": request.form['description'],
+#         "price": request.form['price'],
+#         "quantity": request.form['quantity'],
+#         "image_url": request.form['image_url'],
+#         "id": request.form['id']
+#     }
+
+#     order.Order.update(data)
+#     return redirect('/dashboard')
+
+
+# @app.route('/<int:id>')
+# def show_order(id):
+#     if 'user_id' not in session:
+#         flash('Not logged in!')
+#         return render_template('/index.html')
+#     data = {
+#         "id": id
+#     }
+#     print(order.Order.get_one(data).image_url)
+#     return render_template('view_order.html', order=order.Order.get_one(data))
+
+
+# @app.route('/order/edit/<int:id>')
+# def show_edit_order(id):
+#     if 'user_id' not in session:
+#         flash('Not logged in!')
+#         return render_template('/index.html')
+#     data = {
+#         "id": id
+#     }
+#     return render_template('edit_order.html', order=order.Order.get_one(data), all_categories=category.Category.get_all())
+
+
+# @app.route('/delete/<int:id>')
+# def delete_order(id):
+#     order.Order.delete(id)
+#     return redirect('/dashboard')
